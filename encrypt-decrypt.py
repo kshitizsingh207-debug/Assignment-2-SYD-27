c256705b12802c04372c8639a48b02619fa724c4
Kshitiz Singh <kshitizsingh207@gmail.com>
2026-01-11 06:02:20 +0000

    added decryption

--- encrypt-decrypt.py
+++ encrypt-decrypt.py
@@ -19,13 +19,33 @@ def encrypt_char(ch, shift1, shift2):
         return chr((pos + shift) % 26 + ord('A'))
 
     return ch
+def decrypt_char(ch, shift1, shift2):
+
+    if 'a' <= ch <= 'z':
+        pos = ord(ch) - ord('a')
+        if pos <= 12:
+            shift = shift1 * shift2
+            new_pos = (pos - shift) % 26
+        else:
+            shift = shift1 + shift2
+            new_pos = (pos + shift) % 26
+        return chr(new_pos + ord('a'))
+
+    elif 'A' <= ch <= 'Z':
+        pos = ord(ch) - ord('A')
+        if pos <= 12:
+            new_pos = (pos + shift1) % 26
+        else:
+            new_pos = (pos - (shift2 ** 2)) % 26
+        return chr(new_pos + ord('A'))
+
+    else:
+        return ch
+
+
 def encrypt_file(shift1, shift2):
-    """
-    Encrypt the contents of raw_text.txt and save to encrypted_text.txt
-    """
     raw_file = os.path.join(base_dir, "raw_text.txt")
     enc_file = os.path.join(base_dir, "encrypted_text.txt")
-
     with open(raw_file, "r") as infile, open(enc_file, "w") as outfile:
         for line in infile:
             encrypted_line = ""
@@ -33,6 +53,13 @@ def encrypt_file(shift1, shift2):
                 encrypted_line += encrypt_char(ch, shift1, shift2)
             outfile.write(encrypted_line)
 
-shift1 = int(input("Enter shift1: "))
-shift2 = int(input("Enter shift2: "))
-encrypt_file(shift1, shift2)
+
+def decrypt_file(shift1, shift2):
+    enc_file = os.path.join(base_dir, "encrypted_text.txt")
+    dec_file = os.path.join(base_dir, "decrypted_text.txt")
+    with open(enc_file, "r") as infile, open(dec_file, "w") as outfile:
+        for line in infile:
+            decrypted_line = ""
+            for ch in line:
+                decrypted_line += decrypt_char(ch, shift1, shift2)
+            outfile.write(decrypted_line)
